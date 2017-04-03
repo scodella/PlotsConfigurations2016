919c64e347b0844d493838ac9049f69fc772eeb4
scodella <luca.scodellaro@cern.ch>
2017-04-03 18:58:11 +0200

    improvements on limit submission in batch

--- Configurations/T2tt/submitLimitsLxbatch.sh
+++ Configurations/T2tt/submitLimitsLxbatch.sh
@@ -4,6 +4,6 @@ cat MassPointList.txt | while read line
 do
    if [[ -n $line && "$line" != *"#"* ]]
    then
-       bsub -q cmscaf1nd -o $PWD/jobs $PWD/runPointLimitLxbatch.sh $line
+       bsub -q cmscaf1nd -o $PWD/jobs $PWD/runPointLimitLxbatch.sh $line $PWD
    fi
 done
